f2922f55d9de2c05cc912c1af5bc8d900360ba01
eversoncarvalho <eversoncarvalho@live.com>
2025-06-15 01:56:20 -0300

    new route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://200okish.github.io',
-    base: '/',
+    base: '200okish',
 });
