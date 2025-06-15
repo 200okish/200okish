727fc716f5e7e42c9235650d71fc034d90ea654a
eversoncarvalho <eversoncarvalho@live.com>
2025-06-15 02:01:03 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://200okish.github.io',
-    // Removing the base path to deploy to the root URL
 });
