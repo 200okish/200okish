8004a9321423b3c7f388c0980e34f27e4edf80d0
eversoncarvalho <eversoncarvalho@live.com>
2025-06-15 01:27:29 -0300

    site base

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://200okish.github.io',
+    base: '200okish',
+});
